18a519da8ae3f491eb3f5cdd96b8795e89d29228
asaduzzaman2189 <asaduzzaman2189@gmail.com>
2021-08-24 11:10:13 +0600

    28-1 String comparison using toLowerCase toUpperCase

---
+++ case-convert.js
@@ -0,0 +1,15 @@
+const anthem = 'Amar Sonar Bangla Ami Tomai Valobashi';
+const search = 'valo';
+
+
+const userInput = 'blACkPinK';
+const savedUserName = 'blackpink';
+// console.log(userInput.toLowerCase());
+// console.log(savedUserName.toUpperCase());
+
+if (savedUserName.toLowerCase() == userInput.toLowerCase()) {
+    console.log('user exists');
+}
+else {
+    console.log('false');
+}
